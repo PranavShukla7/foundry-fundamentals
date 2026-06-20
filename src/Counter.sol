daad24b0c11a8b5b1b72657f4b50c0998b66045f
pranavshukla <shuklapranav789@gmail.com>
2026-06-20 23:29:11 +0530

    feat: add ownership and counter controls

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,12 +3,40 @@ pragma solidity ^0.8.24;
 
 contract Counter {
     uint256 public count;
+    address public owner;
+
+    event CountIncremented(uint256 newCount);
+    event CountDecremented(uint256 newCount);
+
+    error NotOwner();
+    error CounterAlreadyZero();
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwner();
+        }
+        _;
+    }
 
     constructor() {
+        owner = msg.sender;
         count = 0;
     }
 
-    function increment() public {
+    function increment() public onlyOwner {
         count++;
+        emit CountIncremented(count);
+    }
+
+    function decrement() public onlyOwner {
+        if (count == 0) {
+            revert CounterAlreadyZero();
+        }
+        count--;
+        emit CountDecremented(count);
+    }
+
+    function reset() public onlyOwner {
+        count = 0;
     }
 }
