97b20d12c574e5031669ada4b71febceb93a9026
pranavshukla <shuklapranav789@gmail.com>
2026-06-27 22:32:19 +0530

    test: prepare gas-price withdrawal coverage

--- fund-me/test/FundMeTest.t.sol
+++ fund-me/test/FundMeTest.t.sol
@@ -11,6 +11,7 @@ contract FundMeTest is Test{
     address USER = makeAddr("pranavshukla");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    //uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         //fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -65,6 +66,7 @@ contract FundMeTest is Test{
         uint256 startingFundMeBalance = address(fundme).balance;
 
         // Act
+        //vm.txGasPrice(GAS_PRICE);
         vm.prank(fundme.getOwner());
         fundme.withdraw();
 
