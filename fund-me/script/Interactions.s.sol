452b3231ce3a550410109887886b94a6e703ac4e
pranavshukla <shuklapranav789@gmail.com>
2026-06-28 22:17:39 +0530

    feat: add fund and withdraw interaction scripts

---
+++ fund-me/script/Interactions.s.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/Fundme.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script{
+    uint256 constant SEND_VALUE = 0.1 ether;
+    function fundFundMe(address mostRecentlyDeployed) public {
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe contract with %s", SEND_VALUE);
+    }
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        vm.startBroadcast();
+        fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
+contract WithdrawFundMe is Script{
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        (bool success,) = mostRecentlyDeployed.call(abi.encodeWithSignature("withdraw()"));
+        require(success, "Call failed");
+        vm.stopBroadcast();
+    }
+    function run() external {
+        address mostRecentlyDeployed = vm.envAddress("MOST_RECENTLY_DEPLOYED");
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
